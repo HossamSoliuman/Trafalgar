3cde834462ba1250695c0602fb9e56cc76ef7ada
Hossam Soliuman <hossamsoliuman@gmail.com>
2025-10-17 18:46:42 +0300

    make daily report sent daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('removeExpireProperty:cron')->daily();
-        $schedule->command('WeekPropertyEmailReport:cron')->everyMinute();
+        $schedule->command('WeekPropertyEmailReport:cron')->dailyAt('6:00');
         $schedule->job(new SendPendingContactEmails)->cron('*/21 * * * *');
     }
 
